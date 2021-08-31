2a780e24468c24c11a5d90f5d8be03914f74df64
yazan abdul hafez ali Alkharabsheh <84713554+yazanabdulhafez@users.noreply.github.com>
2021-08-31 13:10:00 +0300

    Initial commit

---
+++ src/components/FavCrypto.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class FavCrypto extends React.Component {
+  render() {
+    return(
+      <>
+        <h1>Fav Crypto List</h1>
+      </>
+    )
+  }
+}
+
+export default FavCrypto;
